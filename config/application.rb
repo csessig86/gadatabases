5312816118fdb180e40ca4c69474dfadfda181dd
Chris Essig <csessig86@gmail.com>
2015-10-27 10:12:08 -0500

    serve static

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Gadatabases
     # Load files in lib directory
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
+    config.serve_static_assets = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
